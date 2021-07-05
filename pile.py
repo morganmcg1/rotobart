1349efa729afb4e6341ed43045fe72d77cb28b52
Pawan Sasanka Ammanamanchi <Shashi456@users.noreply.github.com>
2021-07-05 23:53:58 +0530

    Update pile.py

--- pile.py
+++ pile.py
@@ -43,6 +43,6 @@ class Pile(datasets.GeneratorBasedBuilder):
                 f = io.TextIOWrapper(f, encoding="utf-8")
                 f = jsonlines.Reader(f)
                 for line in f:
-                    sentence = {"id": id_, "text": line["text"], "pile_set_name": line["meta"]["pile_set_name"]}
-                    yield id_, {"text": sentence["text"]}
+                    sentence = {"text": line["text"], "pile_set_name": line["meta"]["pile_set_name"]}
+                    yield id_, {"id": id_ "text": sentence["text"]}
                     id_ += 1
