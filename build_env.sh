8a90b52115e31ef34cf9981a77080329372652ca
Tyler Thomas <36181311+tylerjthomas9@users.noreply.github.com>
2021-07-10 22:04:45 -0500

    remove git clone from build_env

--- build_env.sh
+++ build_env.sh
@@ -3,4 +3,3 @@ source ~/jax_env/bin/activate
 pip install --upgrade pip
 pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install transformers datasets einops flax aiohttp jsonlines zstandard wandb SentencePiece nltk prefetch_generator torch tensorboard tensorflow
-git clone https://github.com/LouisCastricato/rotobart
