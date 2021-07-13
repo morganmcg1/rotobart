e4eb911d8f76dc411a612a76a218eceea81ba78d
Patrick von Platen <patrick.v.platen@gmail.com>
2021-07-13 21:44:55 +0100

    Correct layer norm weight decay masking for bart-like models

--- run_dnlm_flax.py
+++ run_dnlm_flax.py
@@ -525,7 +525,10 @@ if __name__ == "__main__":
     # accordingly.
     def decay_mask_fn(params):
         flat_params = traverse_util.flatten_dict(params)
-        flat_mask = {path: (path[-1] != "bias" and path[-2:] != ("LayerNorm", "scale")) for path in flat_params}
+        layer_norm_params = [
+            (name, "scale") for name in ["self_attn_layer_norm", "layernorm_embedding", "final_layer_norm"]
+        ]
+        flat_mask = {path: (path[-1] != "bias" and path[-2:] not in layer_norm_params) for path in flat_params}
         return traverse_util.unflatten_dict(flat_mask)
 
     # create optimizer
